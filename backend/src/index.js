e0c5fce5a6d99c57cc955e9646682f62537f4f02
Shivam Sharma <118479614+Shivu-16@users.noreply.github.com>
2025-01-12 17:00:58 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://wechat-client.vercel.app",
     credentials: true,
   })
 );
