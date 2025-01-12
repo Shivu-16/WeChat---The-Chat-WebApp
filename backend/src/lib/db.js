997c234b64e17af2092ad1d50f250ff4268e213e
Shivam Sharma <118479614+Shivu-16@users.noreply.github.com>
2025-01-12 16:50:06 +0530

    Update db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect("mongodb+srv://shivamsharmaug21:SyjzjOXJ8n0zrTZp@cluster0.0jnlm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("MongoDB is connected: " + conn.connection.host);
   } catch (error) {
     console.log("MongoDB connection error: " + error);
